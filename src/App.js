417599bddcfb09d60e0ca3aeea18fcbc9ed385d2
Ryan Moore <ryanmoore@Inkas-MacBook-Pro.local>
2021-03-21 18:42:03 -0700

    Header Complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,71 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-tsparticles";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Particles
+        className="particles-canvas"
+        //id="tsparticles"
+        //init={this.particlesInit}
+        //loaded={this.particlesLoaded}
+        options={{
+          /*background: {
+            color: {
+              value: "#0d47a1",
+            },
+          },*/
+          fpsLimit: 60,
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: .5,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+              value: 50,
+            },
+            opacity: {
+              value: .5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: true,
+              value: 5,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+      <Navbar />
+      <Header />
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from "react"
+import ReactTypingEffect from "react-typing-effect";
+
+const Header = () => {
+    return (
+        <div className ="header-wrapper">
+            <div className ="main-info">
+                <h1>Web Development</h1>
+                <ReactTypingEffect
+                    className="typed-text"
+                    text={["WEB DEVELOPMENT", "PHOTOGRAPHY", "BLOCKCHAIN DEVELOPMENT"]}
+                    speed="100"
+                    eraseSpeed="100"
+                    typingDelay="100"
+                />
+                <a href="#" className="btn-main-offer">Contact Me</a>
+            </div>
+        </div>
+    )
+}
+
+Header.propTypes = {
+
+}
+
+export default Header;
+
